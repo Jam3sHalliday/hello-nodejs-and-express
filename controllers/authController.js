a63d8eed3b5e2e308e69a105811092de13319cb0
Jame3sHalliday <quanggtangg@gmail.com>
2020-11-09 22:18:18 +0700

    #13 loggin user in 1/2 and fix #8 login UI

--- controllers/authController.js
+++ controllers/authController.js
@@ -36,7 +36,7 @@ module.exports.signup_get = (req, res) => {
 module.exports.login_get = (req, res) => {
     res.render('login');
 }
-  
+
 module.exports.signup_post = async (req, res) => {
     const { email, password } = req.body;
 
@@ -54,9 +54,11 @@ module.exports.signup_post = async (req, res) => {
 module.exports.login_post = async (req, res) => {
     const { email, password } = req.body;
 
+
     try {
+        const user = await User.login(email, password);
+        res.status(200).json({ user: user._id });
     } catch (err) {
-        
-        throw new Error(err);
+        res.status(400).json({});
     }
 }
